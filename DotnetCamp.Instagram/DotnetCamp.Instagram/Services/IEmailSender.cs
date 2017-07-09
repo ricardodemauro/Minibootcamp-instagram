e588609b2aef18ab89a7f556becb197fc13168e0
edsonj82 <edsonj82@outlook.com>
2017-07-09 18:07:27 -0300

    upload inicial.

---
+++ DotnetCamp.Instagram/DotnetCamp.Instagram/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCamp.Instagram.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ DotnetCamp.Instagram/DotnetCamp.Instagram/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCamp.Instagram.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
